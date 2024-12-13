807d206e9b6940002554706b30e5ae98421c862d
BBI-M 1047 <bbi-m1047@BBI-M1047.local>
2024-12-13 10:24:59 +0530

    change data of URL

--- lib/features/daily_news/data/data_source/article_remote.dart
+++ lib/features/daily_news/data/data_source/article_remote.dart
@@ -17,9 +17,7 @@ class NewsApiServiceImpl extends NewsApiService {
     final String? api_key = dotenv.env['API_KEY'];
      
     final response = await http.get(Uri.parse(
-        'https://newsapi.org/v2/everything?q=technology&from=2024-11-12&language=en&sortBy=publishedAt&apiKey=$api_key&page=$page&pageSize=$pageSize'));
-     
-       print(response.statusCode);
+        'https://newsapi.org/v2/everything?q=technology&from=2024-13-12&language=en&sortBy=publishedAt&apiKey=$api_key&page=$page&pageSize=$pageSize'));
 
     if (response.statusCode == 200) {
      Map<String, dynamic> data = jsonDecode(response.body);
